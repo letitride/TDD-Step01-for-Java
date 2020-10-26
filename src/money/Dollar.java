062454472702cbac7b38006cbdc2c79597c20d2f
letitride <ichikawa.fumiya@gmail.com>
2020-10-26 20:39:48 +0900

    chapter10 テストに聞いてみる
    timesメソッドの引き上げ
    サブクラスを廃止する為の準備

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -2,12 +2,7 @@ package money;
 
 public class Dollar extends Money {
 
-
     Dollar(int amount, String currency){
         super(amount, currency);
-    }
-
-    Money times(int multiplier){
-        return Money.dollar(amount * multiplier);
     }
 }
--- src/money/Money.java
+++ src/money/Money.java
@@ -1,22 +1,24 @@
 package money;
 
 
-abstract class Money {
+class Money {
 
     protected int amount;
     protected String currency;
-
-    abstract Money times(int multiplier);
 
     Money(int amount, String currency){
         this.amount = amount;
         this.currency = currency;
     }
 
+    Money times(int multiplier){
+        return new Money(amount * multiplier, this.currency);
+    }
+
     public boolean equals(Object object){
         Money money = (Money) object;
         return amount == money.amount
-                && getClass().equals(money.getClass());
+                && currency().equals( money.currency());
     }
 
     static Money dollar(int amount){
@@ -29,5 +31,9 @@ abstract class Money {
 
     String currency(){
         return currency;
+    }
+
+    public String toString(){
+        return amount + " " + currency;
     }
 }
