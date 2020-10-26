4878d31e1ceaf30da2e3bcc82d8a414434702072
letitride <ichikawa.fumiya@gmail.com>
2020-10-26 16:03:13 +0900

    chapter09 歩幅の調整
    通貨リテラルの実装
    コンストラクタの引き上げ

--- src/money/Money.java
+++ src/money/Money.java
@@ -2,9 +2,16 @@ package money;
 
 
 abstract class Money {
+
     protected int amount;
+    protected String currency;
 
     abstract Money times(int multiplier);
+
+    Money(int amount, String currency){
+        this.amount = amount;
+        this.currency = currency;
+    }
 
     public boolean equals(Object object){
         Money money = (Money) object;
@@ -13,10 +20,14 @@ abstract class Money {
     }
 
     static Money dollar(int amount){
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     static Money franc(int amount){
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
+    }
+
+    String currency(){
+        return currency;
     }
 }
